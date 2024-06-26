ae301f0e3171e7761634d3f1c175752ad7e496a9
Rogério Bonfim <rogeriomateus@iclodu.com>
2024-06-25 21:48:56 -0300

    minibanner mobile

--- components/MiniBanner.tsx
+++ components/MiniBanner.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import { MiniBanners, MiniBannersImages } from "@prisma/client"
 import Image from "next/image"
 import { BannersWithImages } from "@/app/page"
+import { useEffect, useState } from "react"
 
 export interface MiniBannerExt extends MiniBanners {
   image: MiniBannersImages | null
@@ -11,22 +14,53 @@ interface MiniBannerProps {
 }
 
 export function MiniBanner({ miniBanner }: MiniBannerProps) {
-  if (!miniBanner.image) return false
+  const [isMobile, setIsMobile] = useState<boolean>(false)
+  const [isTab, setIsTab] = useState<boolean>(false)
 
-  console.log(`minibanner retornado`, miniBanner)
+  if (!miniBanner.image) return null
+  const mobileBanner = miniBanner.image.mobile
+  const tabBanner = miniBanner.image.thumbnail?.lg
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.innerWidth <= 768 && setIsMobile(true)
+      window.innerWidth > 768 && window.innerWidth <= 1024 && setIsTab(true)
+    }
+  }, [])
 
   return (
     <>
       <div className="w-full px-4 my-[40px]">
         <div className="w-full max-w-bisyouContainerHome mx-auto">
           <a href={miniBanner.url} aria-label={miniBanner.title || ""}>
-            <Image
-              alt={miniBanner.image.name}
-              width={1200}
-              height={100}
-              src={miniBanner.image.src}
-              className="w-full"
-            />
+            {isMobile && mobileBanner ? (
+              <Image
+                data-dimension="mobile"
+                alt={miniBanner.image.name}
+                width={1200}
+                height={100}
+                src={mobileBanner}
+                className="w-full"
+              />
+            ) : isTab && tabBanner ? (
+              <Image
+                data-dimension="tab"
+                alt={miniBanner.image.name}
+                width={1200}
+                height={100}
+                src={tabBanner}
+                className="w-full"
+              />
+            ) : (
+              <Image
+                data-dimension="desktop"
+                alt={miniBanner.image.name}
+                width={1200}
+                height={100}
+                src={miniBanner.image.src}
+                className="w-full"
+              />
+            )}
           </a>
         </div>
       </div>
