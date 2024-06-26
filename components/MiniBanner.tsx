182c498dc1a4986078dadbe3da2f6e7253f21385
Rogério Bonfim <rogeriomateus@iclodu.com>
2024-06-25 23:41:46 -0300

    condition

--- components/MiniBanner.tsx
+++ components/MiniBanner.tsx
@@ -26,7 +26,7 @@ export function MiniBanner({ miniBanner }: MiniBannerProps) {
       window.innerWidth <= 768 && setIsMobile(true)
       window.innerWidth > 768 && window.innerWidth <= 1024 && setIsTab(true)
     }
-  }, [])
+  }, [window])
 
   return (
     <>
